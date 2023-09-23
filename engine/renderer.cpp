063df71ab3f84b91ca9b771cc340c27b41a6b2ba
raulRMB <92171903+raulRMB@users.noreply.github.com>
2023-09-23 02:18:00 -0400

    Vulkan Create Instance

--- engine/renderer.cpp
+++ engine/renderer.cpp
@@ -30,5 +30,36 @@ renderer::~renderer()
 
 void renderer::CreateInstance()
 {
+    VkApplicationInfo AppInfo = {};
+    AppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    AppInfo.pApplicationName = "X";
+    AppInfo.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
+    AppInfo.pEngineName = "XEngine";
+    AppInfo.engineVersion = VK_MAKE_VERSION(0, 0, 1);
+    AppInfo.apiVersion = VK_API_VERSION_1_3;
 
+    VkInstanceCreateInfo CreateInfo{};
+    CreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    CreateInfo.pApplicationInfo = &AppInfo;
+
+    std::vector<const char*> InstanceExtensions = std::vector<const char*>();
+    u32 GLFWExtensionCount = 0;
+    const char** GLFWExtensions = glfwGetRequiredInstanceExtensions(&GLFWExtensionCount);
+
+    for (u32 i = 0; i < GLFWExtensionCount; i++)
+    {
+        InstanceExtensions.push_back(GLFWExtensions[i]);
+    }
+
+    CreateInfo.enabledExtensionCount = static_cast<u32>(InstanceExtensions.size());
+    CreateInfo.ppEnabledExtensionNames = InstanceExtensions.data();
+
+    // TODO: Add validation layers
+    CreateInfo.enabledLayerCount = 0;
+    CreateInfo.ppEnabledLayerNames = nullptr;
+
+    if (vkCreateInstance(&CreateInfo, nullptr, &Instance) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to create Vulkan instance!");
+    }
 }
