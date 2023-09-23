bcdfaa027fa5e7b9398c6404a90f377cccb50480
raulRMB <92171903+raulRMB@users.noreply.github.com>
2023-09-22 23:03:02 -0400

    Vendor Setup

---
+++ engine/main.cpp
@@ -0,0 +1,33 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+
+#include <vulkan/vulkan.h>
+#include "../base/defines.h"
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "r", nullptr, nullptr);
+
+    u32 extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
+}
